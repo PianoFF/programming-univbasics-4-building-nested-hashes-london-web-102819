e8f82cbae837329623a07371c333c44402d02f75
Fangfei Chen <fangfei1star@gmail.com>
2019-10-22 18:27:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
 def base_hash
   hash = Hash.new
-	hash{x} = "value"
+	hash["x"] = "value"
 	p hash
 	# Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
